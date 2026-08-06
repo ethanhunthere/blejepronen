09916463606353f27f232c36e98424961d2f6d97
exposerethan <exposerethan@gmail.com>
2026-08-06 19:26:50 +0200

    chore: update AuthShell

--- components/AuthShell.tsx
+++ components/AuthShell.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function AuthShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative min-h-[calc(100vh-64px)] overflow-hidden bg-[#006459]">
+    <div className="relative min-h-[calc(100vh-64px)] overflow-hidden bg-[#006459] flex items-center">
       {/* Ambient light + watermark */}
       <div aria-hidden className="pointer-events-none absolute inset-0">
         <div className="absolute inset-0 bg-[radial-gradient(50%_40%_at_15%_0%,rgba(200,184,130,0.14),transparent_70%)]" />
@@ -15,9 +15,17 @@ export default function AuthShell({ children }: { children: React.ReactNode }) {
           className="absolute -right-24 -bottom-32 w-[420px] max-w-none opacity-[0.06] -rotate-6"
         />
       </div>
-      {/* Brand mark + card, positioned high */}
-      <div className="relative flex flex-col items-center px-4 pt-8 pb-8 lg:pt-12">
-        {children}
+      
+      <div className="relative w-full max-w-[1400px] mx-auto flex flex-col lg:flex-row items-center justify-between h-full w-full">
+        {/* Left side: Auth Form */}
+        <div className="w-full lg:w-1/2 flex items-center justify-center lg:justify-start px-4 py-8 lg:px-12 2xl:px-24">
+          {children}
+        </div>
+        
+        {/* Right side: Animation placeholder */}
+        <div className="hidden lg:flex w-full lg:w-1/2 items-center justify-center h-full min-h-[500px]" id="auth-animation-container">
+          {/* Future animations will be placed here */}
+        </div>
       </div>
     </div>
   )
